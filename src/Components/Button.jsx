ef41f9f31effa477641571a8234c2afb12903f2e
Ethan Brown <eb934053@gmail.com>
2025-04-08 16:06:27 -0500

    Added message representing phone power's status

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,20 +1,26 @@
 export const Button = ({ handler, inputter, numbers, inputNumbers,power, setPower }) => {
   const handlePowerOn = (event) => {
     if (!power) {
+      let phoneScreen = document.querySelector(".phone-screen-off")
+      phoneScreen.textContent = "Powering on..."
       setTimeout(() => {
         setPower(true);
+        phoneScreen.textContent = ""
       }, 1000);
     } else {
+      let phoneScreen = document.querySelector(".phone-screen-on")
+      phoneScreen.textContent = "Powering off..."
       setTimeout(() => {
         setPower(false);
         inputter([]);
+        phoneScreen.textContent = ""
       }, 1000);
     }
   };
 
   const handleCall = () => {
     const msg = new SpeechSynthesisUtterance();
-      msg.text = inputNumbers.join(" . ") + ", is unavailable. Please, try again later."
+      msg.text = inputNumbers.join("  ") + ", is unavailable. Please, try again later."
       window.speechSynthesis.speak(msg);
     };
 
